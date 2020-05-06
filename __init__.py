42eae767f48aaa903fba2869a17e18d2428aed8e
Danny Dasilva <yahchayildasilva@gmail.com>
2020-05-06 17:10:30 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,48 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name" : "Test_addon",
+    "author" : "Danny Dasilva",
+    "description" : "Simple test addon",
+    "blender" : (2, 80, 0),
+    "version" : (0, 0, 1),
+    "location" : "View3D",
+    "warning" : "",
+    "category" : "Generic"
+}
+
+import bpy
+from bpy.props import PointerProperty
+from . test_op import Test_OT_Operator
+from . test_panel import Test_PT_Panel, SimpleOperator, MySettings
+classes = (Test_PT_Panel, Test_OT_Operator, SimpleOperator, MySettings)  
+
+
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+    bpy.types.Scene.my_tool = PointerProperty(type=MySettings)
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+
+    del bpy.types.Scene.my_tool
+
+
+# register, unregister = bpy.utils.register_classes_factory(classes)
