6a0713f07f989552c8d732da74392bfae886cfed
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-05-11 15:32:57 -0400

    add func to draw op

--- draw_op.py
+++ draw_op.py
@@ -17,3 +17,26 @@ class OT_Draw_Operator(Operator):
         self.draw_event = None
 
         self.widgets = []
+    def invoke(self, context, event):
+        self.create_batch()
+
+        args = (self, context)
+        self.register_handlers(args, context)
+
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+    def register_handlers(self, args, context):
+        self.draw_handle = bpy.types.SpaceView3D.draw_handler_add(
+            self.draw_callback_px, args, "WINDOW", "POST_VIEW"
+        )
+        self.draw_event = context.window_manager.event_timer_add(0.1, window = context.window)
+    
+    def unregister_handler(self, context):
+        context.window_manager.event_timer_remove(self.draw_event)
+        bpy.types.SpaceView3D.draw_handler_remove(self.draw_handle, "WINDOW")
+
+        self.draw_handle = None
+        self.draw_event = None
+    def modal(self, context, event):
+        if context.area:
+            context.area
