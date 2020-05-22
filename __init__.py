7a07582b4474b749bf0210e774f54fff97cf965f
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-05-22 12:06:01 -0400

    fixes

--- __init__.py
+++ __init__.py
@@ -16,6 +16,9 @@
 # display pabel for obj spawn and cube draw
 # batch render
 # id name, obj, enable physics, xyz spawn, add another id, outer frame advance and add another id
+
+# add and delete button for add button, replace with obj select, check the names and obj ids if I can
+# see if its possible to dubliicate menus
 bl_info = {
     "name" : "Test_addon",
     "author" : "Danny Dasilva",
@@ -118,28 +121,6 @@ def obj_domain(self, context):
     obj.setxyz(xyz_max, xyz_min)
     obj.run()
 
-
- # vertices = [(0, 0, 4), (4,0,4), (4,0,0),  (4,4,0), (4,4,4), (0,4,4), (0,4,0),(0,0,0), (0,0,4),
-    # (0,4,4), (0,4,0), (4,4,0), (4, 4, 4), (4, 0, 4), (4, 0, 0), (0,0,0)]
-        
-        
-# shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
-# batch = batch_for_shader(shader, 'LINE_STRIP', {'pos': vertices})
-
-# def draw_callback_px():
-#     bgl.glLineWidth(5)
-#     shader.bind()
-#     shader.uniform_float("color", (1, 0, 0, 1))
-#     batch.draw(shader)
-
-
-# if data.draw_handle != None:
-#     bpy.types.SpaceView3D.draw_handler_remove(data.draw_handle, 'WINDOW')
-
-
-# data.draw_handle = bpy.types.SpaceView3D.draw_handler_add(
-#     draw_callback_px, (), "WINDOW", "POST_VIEW"
-#     )
 
 
 
