cf37b6c0c57d71a6fba79c05295cb50cf77b64eb
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-05-27 14:54:16 -0400

    add panels

--- loop_operator.py
+++ loop_operator.py
@@ -4,6 +4,18 @@ import bpy
 def create_custom_operator(i):
     idname = "object.operator_" + i
     
+    def func(self, context):
+        print("Hello World", self.bl_idname)
+        return {'FINISHED'}
+
+    opclass = type("DynOp" + i,
+                   (bpy.types.Operator, ),
+                   {"bl_idname": idname, "bl_label": "Test", "execute": func},
+                   )
+    bpy.utils.register_class(opclass)
+    cls.CustomOp = cls.CustomOp + [opclass]
+    
+    
     
 class MakeCustomOperators(bpy.types.Operator):
     """Create custom operators"""
@@ -15,3 +27,49 @@ class MakeCustomOperators(bpy.types.Operator):
         for n in ['a','b','c']:
             create_custom_operator(n)
         return {'FINISHED'}
+
+
+class Inherit_Panel:
+    bl_space_type = "VIEW_3D"   
+    bl_region_type = "UI"
+    bl_category = "Blender ML"
+    bl_context = "objectmode"  
+    
+    
+class CustomPanel(Inherit_Panel, Panel):
+    bl_label = "My Panel"
+    bl_idname = "OBJECT_PT_CustomPanel"
+    
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        
+        layout.operator("object.add_custom_ops")
+
+
+classes = (
+    CustomPanel,
+    MakeCustomOperators,
+    create_custom_operator,
+    
+    
+
+)
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+    bpy.types.Scene.my_tool = PointerProperty(type=MyProperties)
+
+    #register dynamic creation see if I can place this elsewhere
+    bpy.types.Scene.my_collection = bpy.props.CollectionProperty(type=SceneSettingItem)
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+    del bpy.types.Scene.my_tool
+
+
+if __name__ == "__main__":
+    register()
